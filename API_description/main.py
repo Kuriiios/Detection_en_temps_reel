ab429eb9ddbe7b6f2644f7acc2af8ecb5941bc66
Kuriiios <cyril.leconte07@gmail.com>
2025-12-18 15:47:06 +0100

    added description

--- API_description/main.py
+++ API_description/main.py
@@ -1,28 +1,96 @@
-from fastapi import FastAPI, UploadFile, File
-from loguru import logger
+# DETECTION_en_temps_reel/API_description.py
+
+# --- import ---
+from fastapi import FastAPI, UploadFile, File,  HTTPException
+from fastapi.responses import JSONResponse
+import io
+from PIL import Image
+from pydantic import BaseModel
+from typing import Union
 import uvicorn
 from dotenv import load_dotenv
 import os
+import numpy as np
+from threading import Thread
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+import logging
+from logging.handlers import RotatingFileHandler
+
 load_dotenv()
 
-file = os.path.splitext(os.path.basename(__file__))
+# --- logging --- 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),  # запись в файл
+        logging.StreamHandler()           # вывод в консоль
+    ]
+)
 
-logger.remove()
-logger.add(sink=os.getenv("LOG_PATH") + '_' +file[0] + ".log", rotation="500 MB", level="INFO")
+logger = logging.getLogger(__name__)
 
-
+# --- app ---
 app = FastAPI()
 
-@app.post("/process_image")
-async def process_image(img: UploadFile = File(...)):
-    logger.success("Description reçue")
-    content = await img.read()
-    return {
-        "message": "Description reçue",
-        "filename": img.filename,
-        "size": len(content),
-        "description": "Ceci est une description test"
-    }
+# --- Pydantic models ---
+class ImageDescriptionResponse(BaseModel):
+    status: str
+    message: str
+
+
+# --- global variables, try to load from cache----
+
+
+try:
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+except:
+    processor = None
+    model = None
+ 
+
+
+# --- endpoints ---
+@app.post("/api/image/description", response_model=Union[ImageDescriptionResponse, dict])
+async def describe_image(file: UploadFile = File(...)):
+
+    global processor, model 
+
+# --- verification file type ---    
+    if not file.filename.lower().endswith((".jpg", ".jpeg", ".png")):
+        logger.error("Invalid file type.")
+        raise HTTPException(status_code=422, detail={"status": "422", "message": "Invalid file type. Only JPG/PNG allowed."})
+
+# --- verification exsistance model ---
+    if processor is None or model is None:
+        logger.error("Model isn't loading.")
+        raise HTTPException(status_code=503, detail={"status": "503", "message": "Model isn't loading"})
+    else: 
+        logger.info(f"Model is loaded")
+
+# --- lire une image ---
+    try:
+        contents = await file.read()
+        img = Image.open(io.BytesIO(contents)).convert("RGB")
+        logger.info(f"File is loaded")
+    except Exception as e:
+        logger.error(f"Empty file: {str(e)}.")
+        raise HTTPException(status_code=400, detail={"status": "400", "message": "Invalid file."})
+
+# --- generate description ---
+    try:   
+        inputs = processor(images=img, return_tensors="pt")
+        out = model.generate(**inputs)
+        caption = processor.decode(out[0], skip_special_tokens=True)
+        logger.info(f"Decsription is generated")
+    except Exception as e:
+        logger.error(f"Cannot generate text description: {str(e)}")
+        raise HTTPException(status_code=400, detail={"status": "400", "message": f"Cannot generate text description: {str(e)}"})
+
+    return {"status": "success", "message": caption}
+
 
 if __name__ == "__main__":
     try:
--- API_description/tests/test_api_description.py
+++ API_description/tests/test_api_description.py
@@ -1,2 +1,80 @@
-def test_infrastructure_works():
-    assert True
+import io
+import pytest
+from PIL import Image
+from fastapi.testclient import TestClient
+from API_description.main import app
+from API_description import main
+
+# --- client ---
+client = TestClient(app)
+
+# --- fake model ---
+class DummyProcessor:
+    def __call__(self, *args, **kwargs):
+        return {"pixel_values": "fake"}
+
+    def decode(self, *args, **kwargs):
+        return "a cat"
+
+class DummyModel:
+    def generate(self, *args, **kwargs):
+        return [[1, 2, 3]]
+
+
+# --- test api access ---
+def test_docs_available():
+    response = client.get("/docs")
+    assert response.status_code == 200
+
+# --- test model loading ---
+def test_model_not_loaded(monkeypatch):
+
+    monkeypatch.setattr(main, "processor", None)
+    monkeypatch.setattr(main, "model", None)
+
+    # --- fake image ---
+    img = Image.new("RGB", (10, 10), color="red")
+    file = io.BytesIO()
+    img.save(file, format="JPEG")
+    file.seek(0)
+
+    response = client.post(
+        "/api/image/description",
+        files={"file": ("test.jpg", file, "image/jpeg")}
+    )
+
+    assert response.status_code == 503
+
+# --- test file type ---
+def test_invalid_media_type():
+    file = io.BytesIO(b"not an image")
+    response = client.post(
+        "/api/image/description",
+        files={"file": ("test.txt", file, "text/plain")}
+    )
+
+    assert response.status_code == 422
+
+
+# --- test status and exsitsens message in case of success ---
+def test_success(monkeypatch):
+
+    monkeypatch.setattr(main, "processor", DummyProcessor())
+    monkeypatch.setattr(main, "model", DummyModel())
+
+    # --- fake image ---
+    img = Image.new("RGB", (10, 10), color="red")
+    file = io.BytesIO()
+    img.save(file, format="JPEG")
+    file.seek(0)
+
+    response = client.post(
+        "/api/image/description",
+        files={"file": ("test.jpg", file, "image/jpeg")}
+    )
+
+    assert response.status_code == 200
+    body = response.json()
+
+    assert body["status"] == "success"
+    assert "message" in body
