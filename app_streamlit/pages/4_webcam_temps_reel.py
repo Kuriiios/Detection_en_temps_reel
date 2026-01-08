634cbd67dd9123d407e7464a89b1a85b1cab650d
Hanna Kiyko <kiyko.hanna1984@gmail.com>
2026-01-08 13:36:57 +0100

    add BLIP model integration for image captioning and enhance YOLO processing

--- app_streamlit/pages/4_webcam_temps_reel.py
+++ app_streamlit/pages/4_webcam_temps_reel.py
@@ -4,42 +4,128 @@ import av
 import numpy as np
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+from transformers.models.blip.processing_blip import BlipProcessor
+from transformers.models.blip.modeling_blip import BlipForConditionalGeneration
+
 import sys
 from pathlib import Path
+import time
+import torch
+from PIL import Image
+import logging
+
+# --- logging --- 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),  # lire dans une fichie
+        logging.StreamHandler()           # lire dans une consol
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+# --- configs --- 
+st.set_page_config(page_title="Webcam YOLO – Temps réel", layout="wide")
+
+
+# --- title and logo ---
 
 st.title("Webcam YOLO – Temps réel")
 
+# --- roots ---
 ROOT_DIR = Path(__file__).resolve().parents[2]
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 
+
+# --- loading models ---
 from api_detection.model_loader import load_model
-
-st.set_page_config(layout="wide")
-
 
 @st.cache_resource
 def load_yolo():
     return load_model("x")
 
-model = load_yolo()
+model_yolo = load_yolo()
+
+@st.cache_resource
+def load_blip_model():
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained(
+        "Salesforce/blip-image-captioning-base"
+    ).eval()
+    return processor, model
+
+processor, blip_model = load_blip_model()
 
 class YOLOVideoProcessor(VideoProcessorBase):
+    def __init__(self, confidence):
+        self.confidence = confidence
+        self.last_blip_time = 0
+        self.last_caption = ""
+
+
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-        img = frame.to_ndarray(format="bgr24")
+        try:
+            img = frame.to_ndarray(format="bgr24")
 
-        # YOLO inference
-        results = model(img, conf=0.4, stream=False)
-        img = results[0].plot()
+            # YOLO inference
+            results = model_yolo(img, conf=self.confidence, stream=False)
+            img = results[0].plot()
 
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+            # --- BLIP inference chaque 5 seconde ---
+            current_time = time.time()
 
-webrtc_streamer(
-    key="yolo-realtime",
-    video_processor_factory=YOLOVideoProcessor,
-    media_stream_constraints={
-        "video": True,
-        "audio": False
-    },
-    async_processing=True
+            if (
+                processor is not None
+                and blip_model is not None
+                and current_time - self.last_blip_time >= 5
+            ):
+                self.last_blip_time = current_time
+                # BGR → RGB → PIL
+                image_pil = Image.fromarray(img[:, :, ::-1])
+
+                inputs = processor(image_pil, return_tensors="pt")
+
+                with torch.no_grad():
+                    out = blip_model.generate(**inputs, max_length=50)
+
+                self.last_caption = processor.decode(
+                    out[0], skip_special_tokens=True
+                )
+
+            return av.VideoFrame.from_ndarray(img, format="bgr24")
+        
+        except Exception as e:
+            logging.error("VideoProcessor error")
+            st.error("VideoProcessor error")
+            return frame
+
+# --- slider ---
+confidence = st.slider(
+    "Seuil de confiance",
+    min_value=0.1,
+    max_value=1.0,
+    value=0.5,
+    step=0.05
 )
+
+# 
+col1, col2 = st.columns((3, 1), gap='small', width = "stretch")
+
+with col1:
+
+    ctx = webrtc_streamer(
+        key="yolo-realtime",
+        video_processor_factory=lambda: YOLOVideoProcessor(confidence),
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True
+    )
+
+with col2:
+
+
+    if ctx.video_processor:
+        st.text("Description:")
+        st.write(ctx.video_processor.last_caption)
